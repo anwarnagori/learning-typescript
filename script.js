03422212dab732f6852d2ee0fe810d7b3368876e
anwarnagori <ar1498137@gmail.com>
2024-11-01 10:37:38 -0700

    get started

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+// let user: string = "Anwar";
+// console.log(user)
+// let user:string = "Anwar";
+// user = "Muhammad Anwar";
+// console.log(user)
+// var num:number = 12345;
+// console.log(num);
+// var num:number = 12345;
+// num = 12345678910;
+// console.log(num);
+// function greet(name: string, message:string): void {
+//     console.log(`Hi ${name} ${message}`);
+// }
+// greet(`${prompt('Enter your name')}`, "How are you");
+// function greetUser(name: string, msg: string): string {
+//   return `Assalamu Alaikum ${userName}, ${msg}`;
+// }
+// let userName = "Anwar";
+// let msg = "How are you";
+// function greet() {
+//   console.log(greetUser(userName, msg));
+// }
+// greet();
